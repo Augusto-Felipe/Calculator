0549bd272b46ffe330473cea5ab4dea8a62fbbe3
Felipe Augusto <augusto_felipe@icloud.com>
2022-07-28 23:56:49 -0300

    If statement.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,12 +8,58 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var firstValor: UITextField!
+    @IBOutlet weak var secondvalor: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func sumOperation(_ sender: UIButton) {
+        if let num1 = Double(firstValor.text!) {
+           if let num2 = Double(secondvalor.text!) {
+               
+                let result = num1 + num2
+                resultLabel.text = String(format: "%.2f", result)
+            }
+        }
+        
+    }
+    
+    @IBAction func subOperation(_ sender: UIButton) {
+        if let num1 = Double(firstValor.text!) {
+           if let num2 = Double(secondvalor.text!) {
+               
+                let result = num1 - num2
+                resultLabel.text = String(format: "%.2f", result)
+            }
+        }
+    }
+    
+    @IBAction func divOperation(_ sender: UIButton) {
+        if let num1 = Double(firstValor.text!) {
+           if let num2 = Double(secondvalor.text!) {
+               
+                let result = num1 / num2
+                resultLabel.text = String(format: "%.2f", result)
+            }
+        }
+    }
+    
+    @IBAction func multOperation(_ sender: UIButton) {
+        if let num1 = Double(firstValor.text!) {
+           if let num2 = Double(secondvalor.text!) {
+               
+                let result = num1 * num2
+                resultLabel.text = String(format: "%.2f", result)
+            }
+        }
+    }
+    
 }
 
